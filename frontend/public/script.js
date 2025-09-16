6eff4fae743c1a28e74037e36f52e8db3f80aaed
Maro-dev <107311820+TacYuu@users.noreply.github.com>
2025-09-16 11:58:31 +0800

    Initial

---
+++ frontend/public/script.js
@@ -0,0 +1,5 @@
+// DEPRECATED FILE: Replaced by modular modules in /public/js
+// This file will be removed in a future cleanup. Do not import.
+(function(){
+  if(typeof console!=='undefined') console.warn('[DEPRECATED] public/script.js is unused. Remove any references.');
+})();
